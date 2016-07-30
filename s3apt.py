d1dd4ec23c9babf022045b4b63adbb348f0234e2
Shaun Zinck <shaun.zinck@gmail.com>
2016-07-30 04:18:54 +0000

    Save a hash of .deb packages in metadata
    This will let us only update Packages when it needs to be updated. (when it
    differs with what's actually in the bucket)

--- s3apt.py
+++ s3apt.py
@@ -122,19 +122,45 @@ def get_cached_control_data(deb_obj):
             exists = False
         else:
             raise(e)
-    
+
     if not exists:
         control_data = read_control_data(deb_obj)        
         cache_obj.put(Body=control_data)
 
     return control_data
     
+def get_package_index_hash(prefix):
+    """
+    Returns the md5 hash of the names of all the packages in the index. This can be used
+    to detect if all the packages are represented without having to load a control data cache
+    file for each package.
+    """
+    s3 = boto3.resource('s3')
+    try:
+        package_index_obj = s3.Object(bucket_name=config.APT_REPO_BUCKET_NAME, key=prefix.strip('/') + '/Packages')
+        return package_index_obj.metadata.get('packages-hash', None)
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == '404':
+            return None
+        else:
+            raise(e)
+
+def calc_package_index_hash(deb_names):
+    """
+    Calculates a hash of all the given deb file names. This is deterministic so
+    we can use it for short-circuiting.
+    """
+    md5 = hashlib.md5()
+    md5.update("\n".join(sorted(deb_names)))
+    return md5.hexdigest()
 
 def rebuild_package_index(prefix):
     # Get all .deb keys in directory
     # Get the cache entry
     # build package file
-    packages = []
+    deb_names = []
+    deb_objs = []
+
     filter_prefix = prefix
     if not filter_prefix.endswith('/'):
         filter_prefix += '/'
@@ -144,16 +170,28 @@ def rebuild_package_index(prefix):
     for obj in s3.Bucket(config.APT_REPO_BUCKET_NAME).objects.filter(Prefix=filter_prefix):
         if not obj.key.endswith(".deb"):
             continue
+        deb_objs.append(obj)
+        deb_names.append(obj.key.split('/')[-1])
+
+    # See if we need to rebuild the package index
+    metadata_pkghash = get_package_index_hash(filter_prefix)
+    calcd_pkghash = calc_package_index_hash(deb_names)
+    print("calcd_pkghash=%s, metadata_pkghash=%s" % (calcd_pkghash, metadata_pkghash))
+    if metadata_pkghash == calcd_pkghash:
+        print("PACKAGE INDEX ALREADY UP TO DATE")
+        return
+    
+    pkginfos = []
+    for obj in deb_objs:
         print(obj.key)   
 
         pkginfo = get_cached_control_data(obj)    
         pkginfo = pkginfo + "\n%s\n" % ("Filename: %s" % obj.key)
-        packages.append(pkginfo)
+        pkginfos.append(pkginfo)
 
-    
     package_index_obj = s3.Object(bucket_name=config.APT_REPO_BUCKET_NAME, key=prefix + "/Packages")
     print("Writing package index: %s" % (str(package_index_obj)))
-    package_index_obj.put(Body="\n".join(sorted(packages)))
+    package_index_obj.put(Body="\n".join(sorted(pkginfos)), Metadata={'packages-hash': calcd_pkghash})
 
     print("DONE REBUILDING PACKAGE INDEX")
     
@@ -172,14 +210,24 @@ def lambda_handler(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key']).decode('utf8')
 
-    # For S3 notifications, just make sure we have a cached copy of the control
-    # data.
-    
-    s3 = boto3.resource('s3')
-    deb_obj = s3.Object(bucket_name=bucket, key=key)
 
-    print("S3 Notification of new key. Ensuring cached control data exists: %s" % (str(deb_obj)))
-    get_cached_control_data(deb_obj)
+    # If the Packages index changed or was deleted, try again to rebuild it to
+    # make sure it is up to date.
+    if key.endswith("/Packages"):
+        prefix = "/".join(key.split('/')[0:-1])
+        return rebuild_package_index(prefix)
+
+    if key.endswith(".deb"):
+        # TODO: handle deletions
+        s3 = boto3.resource('s3')
+        deb_obj = s3.Object(bucket_name=bucket, key=key)
+        print("S3 Notification of new key. Ensuring cached control data exists: %s" % (str(deb_obj)))
+        get_cached_control_data(deb_obj)
+
+    # If a package inside this bucket was updated, rebuild the index.
+    if bucket == config.APT_REPO_BUCKET_NAME and key.endswith(".deb"):
+        prefix = "/".join(key.split('/')[0:-1])
+        rebuild_package_index(prefix)
     
     print("DONE")
 
