a3cd0e5f6cc5716587edd297e2dba9f8a99c2d2f
Peter Jochem <pjochem@path-robotics.com>
2022-06-30 17:57:43 -0400

    Update lambda function name
    Looks like the name of the lambda changed. This
    commit changes it to match what is in AWS lambda

--- lambda-publish.sh
+++ lambda-publish.sh
@@ -6,4 +6,4 @@ zip  code.zip s3apt.py config.py
 
 (cd venv/lib/python3.9/site-packages/ ; zip -r ../../../../code.zip *)
 
-aws lambda update-function-code --function-name apt_repo_sync --zip-file fileb://./code.zip --publish
+aws lambda update-function-code --function-name s3apt_repo_maintainer --zip-file fileb://./code.zip --publish
