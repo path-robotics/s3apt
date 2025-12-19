a9ab138bbb02486e43ba1778f4c95126be6589c2
Jeremy Villa <93542390+villapx-path@users.noreply.github.com>
2025-12-19 21:45:08 +0000

    Couple of clerical changes, and update lambda-publish.sh to say 3.14 instead of 3.9

--- lambda-publish.sh
+++ lambda-publish.sh
@@ -2,8 +2,8 @@
 
 # This script will upload a new version of the lambda
 rm code.zip
-zip  code.zip s3apt.py config.py
+zip code.zip s3apt.py config.py
 
-(cd venv/lib/python3.9/site-packages/ ; zip -r ../../../../code.zip *)
+(cd venv/lib/python3.14/site-packages/ ; zip -r ../../../../code.zip *)
 
 aws lambda update-function-code --function-name s3apt_repo_maintainer --zip-file fileb://./code.zip --publish
